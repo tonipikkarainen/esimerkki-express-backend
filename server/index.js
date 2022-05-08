ca1710e77e5be44fed937915566f448e78a44742
Pikkarainen Toni <pikkaton@gradia.fi>
2022-05-08 12:58:56 +0300

    added new logic

--- server/index.js
+++ server/index.js
@@ -6,6 +6,13 @@ const app = express();
 const port = process.env.PORT || 3002;
 let count = 0;
 
+function checkNumber(input) {
+  let number = parseInt(input);
+  let rightAnswer = Math.floor(Math.random() * 10);
+  const answer = number === rightAnswer ? "Oikein - hienoa!" : "Väärin - oikea numero oli " + rightAnswer;
+  return answer;
+}
+
 app.use(cors());
 
 app.get('/', (req, res) => {
@@ -16,10 +23,11 @@ app.get('/api/:id', (req, res) => {
     console.log(req.params);
     console.log("here")
     const { id } = req.params;
+    const answer = checkNumber(id);
 
     res.send({
         resp: "ok",
-        message: "Hello you id: " + id + " number " + count
+        message: answer
     });
     count++;
   });
